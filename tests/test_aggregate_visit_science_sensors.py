76b7e1767d7bf450cf8f194c7ee598e686c204a7
J. Bryce Kalmbach <jbkalmbach@gmail.com>
2024-10-29 13:08:44 -0700

    Implement pipeline run for tests.

---
+++ tests/test_aggregate_visit_science_sensors.py
@@ -0,0 +1,60 @@
+import os
+
+from lsst.daf.butler import Butler
+from lsst.donut.viz import AggregateZernikeTablesTask, AggregateZernikeTablesTaskConfig
+from lsst.ts.wep.utils import (
+    getModulePath,
+    runProgram,
+    writeCleanUpRepoCmd,
+    writePipetaskCmd,
+)
+from lsst.utils.tests import TestCase
+
+
+class TestAggregateZernikeTablesTask(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        wep_module_dir = getModulePath()
+        cls.test_data_dir = os.path.join(wep_module_dir, "tests", "testData")
+        cls.test_repo_dir = os.path.join(cls.test_data_dir, "gen3TestRepo")
+
+        cls.butler = Butler(cls.test_repo_dir)
+        cls.test_run_name = "test_run_1"
+        registry = cls.butler.registry
+        collections_list = list(registry.queryCollections())
+        if cls.test_run_name in collections_list:
+            clean_up_cmd = writeCleanUpRepoCmd(cls.test_repo_dir, cls.test_run_name)
+            runProgram(clean_up_cmd)
+
+        collections = "refcats/gen2,LSSTCam/calib,LSSTCam/raw/all"
+        instrument = "lsst.obs.lsst.LsstCam"
+        cls.camera_name = "LSSTCam"
+        test_pipeline = os.path.join(
+            os.getenv("DONUT_VIZ_DIR"),
+            "pipelines",
+            "scienceDirectDetectPipeline.yaml",
+        )
+
+        pipe_cmd = writePipetaskCmd(
+            cls.test_repo_dir,
+            cls.test_run_name,
+            instrument,
+            collections,
+            pipelineYaml=test_pipeline,
+        )
+        # Make sure we are using the right exposure+detector combinations
+        pipe_cmd += ' -d "exposure IN (4021123106001, 4021123106002) AND '
+        pipe_cmd += 'detector NOT IN (191, 192, 195, 196, 199, 200, 203, 204)"'
+        runProgram(pipe_cmd)
+
+    @classmethod
+    def tearDownClass(cls):
+        clean_up_cmd = writeCleanUpRepoCmd(cls.test_repo_dir, cls.test_run_name)
+        runProgram(clean_up_cmd)
+
+    def setUp(self):
+        self.config = AggregateZernikeTablesTaskConfig()
+        self.task = AggregateZernikeTablesTask(config=self.config)
+
+    def testValidateConfigs(self):
+        self.assertEqual(1, 1)
