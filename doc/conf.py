a486c690356cd11de299ff884a0c9c4425c1d2f0
J. Bryce Kalmbach <jbkalmbach@gmail.com>
2026-09-01 16:22:14 -0700

    Update documentation build.

--- doc/conf.py
+++ doc/conf.py
@@ -21,16 +21,3 @@
 
 import lsst.donut.viz  # noqa
 from documenteer.conf.guide import *  # type: ignore # noqa
-
-project = "donut_viz"
-html_theme_options["logotext"] = project  # type: ignore # noqa
-html_title = project
-html_short_title = project
-doxylink = {}  # type: ignore
-
-
-# Support the sphinx extension of mermaid
-extensions = [
-    "sphinxcontrib.mermaid",  # type: ignore # noqa
-    "sphinx_automodapi.automodapi",  # type: ignore # noqa
-]
